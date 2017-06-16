d93373154f47a5c0d284e470d4bca0d3fe00d975
Aashir <kaashir9@gmail.com>
2017-06-16 12:12:26 +0500

    java calculator

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,18 @@
+
+package calculator;
+
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+String a="12",b="12";
+double v1=Double.parseDouble(a);
+double v2=Double.parseDouble(b);
+        System.out.println("addition is: "+(a+b));
+
+    }
+    
+}
